363f2d306c925ec9bd161bb713e1f5f56bb14bde
Elaine Farrow <Elaine.Farrow@ed.ac.uk>
2020-07-14 13:18:46 +0100

    Remove references to Connector in utils - no longer used here.

--- src/patentdata/opstools/OpsConfigHelper.java
+++ src/patentdata/opstools/OpsConfigHelper.java
@@ -1,7 +1,6 @@
 package patentdata.opstools;
 
 import patentdata.utils.Config;
-import patentdata.utils.Connector;
 
 import org.apache.commons.codec.binary.Base64;
 
@@ -12,7 +11,6 @@ import org.apache.commons.codec.binary.Base64;
  */
 public class OpsConfigHelper {
 
-    private final Connector connector;
     private final Logger logger;
     private final String authUrl;
     private final String authString;
@@ -21,19 +19,11 @@ public class OpsConfigHelper {
 
     public OpsConfigHelper(String path) throws Exception {
         Config config = new Config(path);
-        connector = new Connector(config);
         workingDirName = config._config.WorkingDir;
         logger = new Logger(workingDirName);
         authString = Base64.encodeBase64String((config._config.ConsumerKey + ":" + config._config.ConsumerSecret).getBytes());
         authUrl = config._config.AuthenURL;
         serviceUrl = config._config.ServiceURL.replaceAll("(?i)(?<!(http:|https:))/+", "/");
-    }
-
-    /**
-     * Gets the connector.
-     */
-    public Connector getConnector() {
-        return connector;
     }
 
     /**
