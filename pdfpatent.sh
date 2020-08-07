b1f7c38f839004f2cda55bf3e77bc560e2ed0a33
Elaine Farrow <Elaine.Farrow@ed.ac.uk>
2020-08-07 12:05:56 +0100

    Add start and end year as script parameters.

--- pdfpatent.sh
+++ pdfpatent.sh
@@ -4,15 +4,17 @@
 
 COUNTRY=${1:-HR}
 COMMAND=${2:-report}
+YEAR_START=${3:-1994}
+YEAR_END=${4:-2019}
 
 SCRIPTDIR=${HOME}/tmp
 SCRIPT=${SCRIPTDIR}/${COMMAND}-script-`date -Iseconds`.txt
 
 mkdir -p ${SCRIPTDIR}
 
-for (( year=1994; year<2020; year++ ))
+for (( year=${YEAR_START}; year<=${YEAR_END}; year++ ))
 do
-  echo ${COUNTRY} $year ${COMMAND} >> ${SCRIPT}
+    echo ${COUNTRY} $year ${COMMAND} >> ${SCRIPT}
 done
 
 nice -n10 java -jar build/libs/pdfpatents.jar -f ${SCRIPT}
