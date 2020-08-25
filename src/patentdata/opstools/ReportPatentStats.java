235134db8e70213b05db4d7d612ea9ffaee27ac7
Elaine Farrow <Elaine.Farrow@ed.ac.uk>
2020-08-25 16:52:45 +0100

    Update progress report to include number of downloads.

--- src/patentdata/opstools/ReportPatentStats.java
+++ src/patentdata/opstools/ReportPatentStats.java
@@ -115,24 +115,47 @@ public class ReportPatentStats {
             if (missingInfo > 0) {
                 System.out.println(String.format("%8d with missing information", missingInfo));
             }
+            StringBuilder allDownloads = new StringBuilder();
             for (String lang : OpsResultProcessor.sort(anyText)) {
+                StringBuilder downloads = new StringBuilder();
                 System.out.println(String.format("  text in language \"%s\":", lang));
                 if (titles.containsKey(lang)) {
                     System.out.println(String.format("%8d with title", titles.get(lang)));
+                    long count = writer.countEntries(lang, PatentResultWriter.TITLE_FILE);
+                    if (count > 0) {
+                        downloads.append(String.format("%8d titles\n", count));
+                    }
                 }
                 if (abstracts.containsKey(lang)) {
                     System.out.println(String.format("%8d with abstract", abstracts.get(lang)));
+                    long count = writer.countEntries(lang, PatentResultWriter.ABSTRACT_FILE);
+                    if (count > 0) {
+                        downloads.append(String.format("%8d abstracts\n", count));
+                    }
                 }
                 if (claims.containsKey(lang)) {
                     System.out.println(String.format("%8d with claims", claims.get(lang)));
+                    long count = writer.countEntries(lang, PatentResultWriter.CLAIM_FILE);
+                    if (count > 0) {
+                        downloads.append(String.format("%8d claims\n", count));
+                    }
                 }
                 if (descriptions.containsKey(lang)) {
                     System.out.println(String.format("%8d with description", descriptions.get(lang)));
+                    long count = writer.countEntries(lang, PatentResultWriter.DESCRIPTION_FILE);
+                    if (count > 0) {
+                        downloads.append(String.format("%8d descriptions\n", count));
+                    }
+                }
+                if (downloads.length() > 0) {
+                    allDownloads.append(String.format("  downloaded in language \"%s\":\n", lang));
+                    allDownloads.append(downloads);
                 }
                 if (allText.containsKey(lang)) {
                     System.out.println(String.format("%8d with all text parts", allText.get(lang)));
                 }
             }
+            System.out.print(allDownloads);
             if (withNeededImages > 0) {
                 System.out.println(String.format("  PDF pages:"));
                 System.out.println(String.format("%8d PDF pages needed (mean %.1f per patent)", totalNeededImages, averageImages));
@@ -140,6 +163,7 @@ public class ReportPatentStats {
                 System.out.println(String.format("%8d PDF pages still to download", totalNeededImages - totalDownloadedImages));
             }
             if (totalPatents > patentsProcessed) {
+                System.out.println(String.format("  to do:"));
                 System.out.println(String.format("%8d patents still to check", totalPatents - patentsProcessed));
             }
         }
