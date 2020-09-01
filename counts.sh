b5b1e3862b65b0a28fee2f9e0e680d2b27d5432c
Elaine Farrow <Elaine.Farrow@ed.ac.uk>
2020-09-01 17:20:30 +0100

    Fix typos and improve readability.

--- counts.sh
+++ counts.sh
@@ -18,27 +18,24 @@ do
         continue
     fi
     INFOFILE=`ls "${YEARDIR}"/*-info.txt`
-    TITLEFILE="${YEARDIR}/${COUNTRY}-${COUNTRY}-${YEAR}-title.tab"
-    ABSTRACTFILE="${YEARDIR}/${COUNTRY}-${COUNTRY}-${YEAR}-abstract.tab"
-    CLAIMSFILE="${YEARDIR}/${COUNTRY}-${COUNTRY}-${YEAR}-claims.tab"
-    DESCRIPTIONFILE="${YEARDIR}/${COUNTRY}-${COUNTRY}-${YEAR}-title.tab"
 
-    # find downloaded entries
-    ENTRIES=`wc -l ${INFOFILE} | cut -d' ' -f1`
-    TITLE_DOWNLOADS=`find "${YEARDIR}" -maxdepth 1 -type f -name "${COUNTRY}-${COUNTRY}-${YEAR}-title.tab" | xargs wc -l | cut -d' ' -f1`
-    ABSTRACT_DOWNLOADS=`find "${YEARDIR}" -maxdepth 1 -type f -name "${COUNTRY}-${COUNTRY}-${YEAR}-abstract.tab" | xargs wc -l | cut -d' ' -f1`
-    CLAIMS_DOWNLOADS=`find "${YEARDIR}" -maxdepth 1 -type f -name "${COUNTRY}-${COUNTRY}-${YEAR}-claims.tab" | xargs wc -l | cut -d' ' -f1`
-    DESCRIPTION_DOWNLOADS=`find "${YEARDIR}" -maxdepth 1 -type f -name "${COUNTRY}-${COUNTRY}-${YEAR}-description.tab" | xargs wc -l | cut -d' ' -f1`
+    # count downloaded entries in the main language
+    FILE_PREFIX="${COUNTRY}-${COUNTRY}-${YEAR}"
+    TITLE_DOWNLOADS=`find "${YEARDIR}" -maxdepth 1 -type f -name "${FILE_PREFIX}-title.tab" | xargs wc -l | cut -d' ' -f1`
+    ABSTRACT_DOWNLOADS=`find "${YEARDIR}" -maxdepth 1 -type f -name "${FILE_PREFIX}-abstract.tab" | xargs wc -l | cut -d' ' -f1`
+    CLAIM_DOWNLOADS=`find "${YEARDIR}" -maxdepth 1 -type f -name "${FILE_PREFIX}-claim.tab" | xargs wc -l | cut -d' ' -f1`
+    DESCRIPTION_DOWNLOADS=`find "${YEARDIR}" -maxdepth 1 -type f -name "${FILE_PREFIX}-desc.tab" | xargs wc -l | cut -d' ' -f1`
     PDF_DOWNLOADS=`ls "${YEARDIR}" | grep -c "\-1.pdf"`
 
-    # find info file entries
+    # count info file entries
+    ENTRY_COUNT=`wc -l ${INFOFILE} | cut -d' ' -f1`
     TITLE_COUNT=`cut -f4 "${INFOFILE}" | grep "${COUNTRY}" | wc -l`
     ABSTRACT_COUNT=`cut -f5 "${INFOFILE}" | grep "${COUNTRY}" | wc -l`
     CLAIM_COUNT=`cut -f6 "${INFOFILE}" | grep "${COUNTRY}" | wc -l`
     DESCRIPTION_COUNT=`cut -f7 "${INFOFILE}" | grep "${COUNTRY}" | wc -l`
     PDF_COUNT=`cut -f8 "${INFOFILE}" | grep "${COUNTRY}" | wc -l`
 
-    # find rows with parts as text and with PDFs available
+    # count entries with parts as text and with PDFs available
     TITLE_MATCH=`cut -f4-8 "${INFOFILE}" | grep -vP "${TEXT_PATTERN}" | cut -f1,5  | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
     ABSTRACT_MATCH=`cut -f4-8 "${INFOFILE}" | grep -vP "${TEXT_PATTERN}" | cut -f2,5  | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
     CLAIM_MATCH=`cut -f4-8 "${INFOFILE}" | grep -vP "${TEXT_PATTERN}" | cut -f3,5  | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
@@ -46,7 +43,7 @@ do
     PDF_MATCH=`cut -f4-8 "${INFOFILE}" | grep -vP "${TEXT_PATTERN}" | cut -f5  | grep -P "${COUNTRY}" | wc -l`
 
     # print output
-    printf "${YEAR}: ${ENTRIES} entries\n"
+    printf "${YEAR}: ${ENTRY_COUNT} entries\n"
     printf "%6d / %-5d titles have PDFs\n" "${TITLE_MATCH}" "${TITLE_COUNT}"
     printf "%6d / %-5d abstracts have PDFs\n" "${ABSTRACT_MATCH}" "${ABSTRACT_COUNT}"
     printf "%6d / %-5d claims have PDFs\n" "${CLAIM_MATCH}" "${CLAIM_COUNT}"
