b4b2a64aa3a342e4014b9d0e42a1ac4bf5773814
Elaine Farrow <Elaine.Farrow@ed.ac.uk>
2020-09-04 17:59:05 +0100

    Take the PDF page limit into account, with default value 25.
    Use a temporary file to filter the patents before counting.

--- counts.sh
+++ counts.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-# ./counts.sh NO 2018 2019 | more
+# ./counts.sh NO 2018 | more
+# (PDF_PAGE_LIMIT=10 ./counts.sh HR 1996)
 
 COUNTRY=${1:-HR}
 YEAR_START=${2:-1994}
@@ -8,6 +9,9 @@ YEAR_END=${3:-${2:-2019}}
 
 SCRIPTDIR="${HOME}/tmp"
 INFODIR="${INFODIR:-/fs/loki0/data/pdfpatents}"
+PDF_PAGE_LIMIT="${PDF_PAGE_LIMIT:-25}"
+
+mkdir -p "${SCRIPTDIR}"
 
 TEXT_PATTERN='^[^[:space:]]+\t[^[:space:]]+\t[^[:space:]]+\t[^[:space:]]+'
 for (( YEAR="${YEAR_START}"; YEAR<="${YEAR_END}"; YEAR++ ))
@@ -18,6 +22,7 @@ do
         continue
     fi
     INFOFILE=`ls "${YEARDIR}"/*-info.txt`
+    TMPFILE="${SCRIPTDIR}/`date -Idate`-${COUNTRY}-${YEAR}-info.txt"
 
     # count downloaded entries in the main language
     FILE_PREFIX="${COUNTRY}-${COUNTRY}-${YEAR}"
@@ -27,20 +32,23 @@ do
     DESCRIPTION_DOWNLOADS=`find "${YEARDIR}" -maxdepth 1 -type f -name "${FILE_PREFIX}-desc.tab" | xargs wc -l | cut -d' ' -f1`
     PDF_DOWNLOADS=`ls "${YEARDIR}" | grep -c "\-1.pdf"`
 
-    # count info file entries
-    ENTRY_COUNT=`wc -l ${INFOFILE} | cut -d' ' -f1`
+    # count info file entries and text in the main language
+    ENTRY_COUNT=`wc -l "${INFOFILE}" | cut -d' ' -f1`
     TITLE_COUNT=`cut -f4 "${INFOFILE}" | grep "${COUNTRY}" | wc -l`
     ABSTRACT_COUNT=`cut -f5 "${INFOFILE}" | grep "${COUNTRY}" | wc -l`
     CLAIM_COUNT=`cut -f6 "${INFOFILE}" | grep "${COUNTRY}" | wc -l`
     DESCRIPTION_COUNT=`cut -f7 "${INFOFILE}" | grep "${COUNTRY}" | wc -l`
     PDF_COUNT=`cut -f8 "${INFOFILE}" | grep "${COUNTRY}" | wc -l`
 
-    # count entries with parts as text and with PDFs available
-    TITLE_MATCH=`cut -f4-8 "${INFOFILE}" | grep -vP "${TEXT_PATTERN}" | cut -f1,5  | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
-    ABSTRACT_MATCH=`cut -f4-8 "${INFOFILE}" | grep -vP "${TEXT_PATTERN}" | cut -f2,5  | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
-    CLAIM_MATCH=`cut -f4-8 "${INFOFILE}" | grep -vP "${TEXT_PATTERN}" | cut -f3,5  | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
-    DESCRIPTION_MATCH=`cut -f4-8 "${INFOFILE}" | grep -vP "${TEXT_PATTERN}" | cut -f4,5  | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
-    PDF_MATCH=`cut -f4-8 "${INFOFILE}" | grep -vP "${TEXT_PATTERN}" | cut -f5  | grep -P "${COUNTRY}" | wc -l`
+    # locate entries lacking at least one text part; filter by max PDF page limit
+    cut -f4- "${INFOFILE}" | grep -vP "${TEXT_PATTERN}" | awk -F "\t" -v limit="${PDF_PAGE_LIMIT}" '$6 <= limit' > "${TMPFILE}"
+
+    # count filtered entries with parts as text and with PDFs available
+    TITLE_MATCH=`cut -f1,5 "${TMPFILE}" | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
+    ABSTRACT_MATCH=`cut -f2,5 "${TMPFILE}" | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
+    CLAIM_MATCH=`cut -f3,5 "${TMPFILE}" | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
+    DESCRIPTION_MATCH=`cut -f4,5 "${TMPFILE}" | grep -P "${COUNTRY}.*${COUNTRY}" | wc -l`
+    PDF_MATCH=`cut -f6 "${TMPFILE}" | awk '$1 > 0' | wc -l`
 
     # print output
     printf "${COUNTRY} ${YEAR}: ${ENTRY_COUNT} entries\n"
@@ -48,10 +56,12 @@ do
     printf "%6d / %-5d abstracts have PDFs\n" "${ABSTRACT_MATCH}" "${ABSTRACT_COUNT}"
     printf "%6d / %-5d claims have PDFs\n" "${CLAIM_MATCH}" "${CLAIM_COUNT}"
     printf "%6d / %-5d descriptions have PDFs\n" "${DESCRIPTION_MATCH}" "${DESCRIPTION_COUNT}"
-    printf "%6d / %-5d PDFs wanted\n" "${PDF_MATCH}" "${PDF_COUNT}"
+    printf "%6d / %-5d PDFs are wanted\n" "${PDF_MATCH}" "${PDF_COUNT}"
     printf "%6d titles downloaded\n" "${TITLE_DOWNLOADS}"
     printf "%6d abstracts downloaded\n" "${ABSTRACT_DOWNLOADS}"
     printf "%6d claims downloaded\n" "${CLAIM_DOWNLOADS}"
     printf "%6d descriptions downloaded\n" "${DESCRIPTION_DOWNLOADS}"
     printf "%6d PDFs downloaded\n" "${PDF_DOWNLOADS}"
+
+    rm "${TMPFILE}"
 done
