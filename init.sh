029dd1d77328f50330c1170328ec358f2c611283
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-02-19 19:32:48 +0000

    Support CSD3

--- init.sh
+++ init.sh
@@ -8,6 +8,18 @@ if [[ "$(hostname)" =~ "cirrus" ]]; then
 		java/jdk-14.0.1
 fi
 
+if [[ "$(hostname -A)" =~ "hpc.cam.ac.uk" ]]; then
+	module purge && module load \
+		rhel7/global \
+		slurm \
+		parallel \
+		cmake \
+		gcc/8 `# CUDA10.2 can't deal with gcc > 8` \
+		binutils-2.31.1-gcc-5.4.0-uyyspmn `# Nick: newer binutils is better` \
+		intel/mkl/2020.2 \
+		cuda/10.2 `# Marian doesn't support 11 properly yet`
+fi
+
 export PREFIX=$(dirname $(realpath $0))
 BINDIR=$PREFIX/bin
 export PATH=$BINDIR:$PATH
