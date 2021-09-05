3c1716ac244960589febcb180f6accc0f626e774
Elaine Farrow <Elaine.Farrow@ed.ac.uk>
2021-09-05 12:40:23 +0100

    Combine text parts of patents in EPO and USPTO format too.
    Allow the output files to optionally be saved to a separate output
    directory. If not provided, output files will be added into the file
    structure of the input directory as before.

--- combine_text.py
+++ combine_text.py
@@ -14,10 +14,16 @@ DEFAULT_START_YEAR = 1980
 
 # patent parts in order
 FILE_TYPES = OrderedDict()
-FILE_TYPES.update({'Title' : 'title'})
-FILE_TYPES.update({'Abstract' : 'abstract'})
-FILE_TYPES.update({'Description' : 'desc'})
-FILE_TYPES.update({'Claims' : 'claim'})
+FILE_TYPES.update({'Title' : ['title', 'Title']})
+FILE_TYPES.update({'Abstract' : ['abstract', 'Abstract']})
+FILE_TYPES.update({'Description' : ['desc', 'Description']})
+FILE_TYPES.update({'Claims' : ['claim', 'Claim']})
+
+ID_FIELD = 'ID'
+DATE_FIELD = 'date'
+CONTENT_FIELD = 'content'
+CONTENT_FIELD_X = CONTENT_FIELD + '_x'
+CONTENT_FIELD_Y = CONTENT_FIELD + '_y'
 
 def combine_text(args):
     for year in range(args.start, args.end+1):
@@ -28,55 +34,115 @@ def combine_text(args):
             print('{} done'.format(year))
 
 def combine_text_by_year(args, year):
-    session = '{}-{}'.format(args.country, year)
-    yeardir = '{}/{}'.format(args.datadir, session)
-    infofile = '{}/{}-info.txt'.format(yeardir, session)
-    if not os.path.isdir(yeardir) or not os.path.isfile(infofile):
-        return
     # find the downloaded text entries
-    langs = defaultdict(dict)
-    file_pattern = '{}-*-{}-*.tab'.format(args.country, year)
-    for p in Path(yeardir).glob(file_pattern):
-        _, lang, _, filetype = p.stem.split('-')
-        langs[lang][filetype] = str(p)
+    langs = get_files_by_language(args, year)
     # combine the downloaded text entries for each language
     for lang in langs:
         if args.verbose:
             print('Language: {}'.format(lang))
         filenames = []
-        for t, f in FILE_TYPES.items():
-            filename = langs[lang].get(f)
+        for label in FILE_TYPES:
+            filename = langs[lang].get(label)
             if filename is not None:
                 filenames.append(filename)
             if args.verbose:
-                print('  {}:\t{}'.format(t, filename))
+                print('  {}:\t{}'.format(label, filename))
         df = combine_text_by_language(filenames)
         # save the combined data
         if df is not None:
-            output_filename = '{}/{}-{}-{}-text.tab'.format(yeardir, args.country, lang, year)
-            with open(output_filename, 'w') as f:
-                df.to_csv(f, sep='\t', quoting=csv.QUOTE_NONE, header=False, index=False)
+            output_dir = get_output_dir(args, year)
+            os.makedirs(output_dir, exist_ok=True)
+            output_filename = get_output_filename(args, lang, year)
+            write_data(df, '{}/{}'.format(output_dir, output_filename))
             if args.verbose:
                 print('  Saved {}'.format(output_filename))
 
 def combine_text_by_language(input_filenames):
-    CONTENT_FIELD = 'content'
-    CONTENT_FIELD_X = CONTENT_FIELD + '_x'
-    CONTENT_FIELD_Y = CONTENT_FIELD + '_y'
     df = None
     for input_filename in input_filenames:
-        df2 = pd.read_csv(input_filename, sep='\t', quoting=csv.QUOTE_NONE, names=['ID', 'date', CONTENT_FIELD])
+        df2 = fix_format_errors(read_data(input_filename))
         if df is None:
             df = df2
         else:
-            df = df.merge(df2, how='outer', sort=True, on=['ID', 'date'], validate='1:1')
+            df = df.merge(df2, how='outer', sort=True, on=[ID_FIELD, DATE_FIELD], validate='1:1')
             df[CONTENT_FIELD] = df[[CONTENT_FIELD_X, CONTENT_FIELD_Y]].apply(join_text, axis=1)
             df = df.drop(columns=[CONTENT_FIELD_X, CONTENT_FIELD_Y])
     return df
 
+def fix_format_errors(df):
+    # fix some common file format errors
+    TMP_FIELD = 'tmp'
+    df[TMP_FIELD] = df[DATE_FIELD].str[8:]
+    df[DATE_FIELD] = df[DATE_FIELD].str[:8]
+    df[CONTENT_FIELD] = df[[TMP_FIELD, CONTENT_FIELD]].apply(join_text, axis=1)
+    df = df.drop(columns=[TMP_FIELD])
+    df = df.drop_duplicates([ID_FIELD, DATE_FIELD])
+    return df
+
 def join_text(texts):
-    # skip missing values
-    return '<br>'.join((t for t in texts if pd.notna(t)))
+    # skip missing and empty values
+    return '<br>'.join((t for t in texts if t and pd.notna(t)))
+
+def get_files_by_language(args, year):
+    langs = defaultdict(dict)
+    input_dir = get_input_dir(args, year)
+    if os.path.isdir(input_dir):
+        file_pattern = get_file_pattern(args, year)
+        for p in Path(input_dir).glob(file_pattern):
+            lang, _, suffix = p.stem.split('-')[-3:]
+            filetype = get_file_type(suffix)
+            if filetype is not None:
+                langs[lang][filetype] = str(p)
+    return langs
+
+def get_input_dir(args, year):
+    if args.epo:
+        input_dir = 'epo'
+    elif args.us:
+        input_dir = 'uspto'
+    else:
+        input_dir = '{}-{}'.format(args.country, year)
+    return '{}/{}'.format(args.datadir, input_dir)
+
+def get_file_pattern(args, year):
+    if args.epo:
+        file_pattern = '*/{}-{}-*.tab'.format(args.country, year)
+    elif args.us:
+        file_pattern = '*/USPTO-{}-{}-*.tab'.format(args.country, year)
+    else:
+        file_pattern = '{}-*-{}-*.tab'.format(args.country, year)
+    return file_pattern
+
+def get_file_type(suffix):
+    for label, types in FILE_TYPES.items():
+        if suffix in types:
+            return label
+    return None
+
+def get_output_dir(args, year):
+    if args.outdir:
+        return args.outdir
+    output_dir = get_input_dir(args, year)
+    if args.epo or args.us:
+        output_dir = '{}/text'.format(output_dir)
+    return output_dir
+
+def get_output_filename(args, lang, year):
+    if args.epo:
+        output_filename = 'EP-{}-{}-text.tab'.format(lang, year)
+    elif args.us:
+        output_filename = 'USPTO-{}-{}-text.tab'.format(lang, year)
+    else:
+        output_filename = '{}-{}-{}-text.tab'.format(args.country, lang, year)
+    return output_filename
+
+def read_data(filename):
+    return pd.read_csv(filename, sep='\t', dtype='str', quoting=csv.QUOTE_NONE, names=[ID_FIELD, DATE_FIELD, CONTENT_FIELD])
+
+def write_data(df, filename):
+    df = df[[ID_FIELD, DATE_FIELD, CONTENT_FIELD]]
+    with open(filename, 'w') as f:
+        df.to_csv(f, sep='\t', quoting=csv.QUOTE_NONE, header=False, index=False)
 
 def check_country(value):
     if len(value) == 2 and value.isalpha():
@@ -89,6 +155,13 @@ def check_year(value):
         raise argparse.ArgumentTypeError('%s is out of range' % value)
     return ivalue
 
+def check_dir_path(value):
+    message = 'not a directory'
+    if os.path.isdir(value):
+        return value
+    else:
+        raise argparse.ArgumentTypeError(message)
+
 def main():
     startyear = DEFAULT_START_YEAR
     endyear = datetime.datetime.now().year-1
@@ -99,6 +172,9 @@ def main():
     parser.add_argument('country', metavar='country-code', help='Country code', type=check_country)
     parser.add_argument('start', metavar='start-year', nargs='?', help='First year of patents to process', type=check_year)
     parser.add_argument('end', metavar='end-year', nargs='?', help='Last year of patents to process', type=check_year)
+    parser.add_argument('--epo', help='EPO data', action='store_true')
+    parser.add_argument('--us', '--uspto', help='USPTO data', action='store_true')
+    parser.add_argument('--outdir', metavar='output-dir', help='Output directory', type=check_dir_path)
     parser.add_argument('--datadir', help='Directory with data files to process', default=datadir)
     parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true')
     args = parser.parse_args()
