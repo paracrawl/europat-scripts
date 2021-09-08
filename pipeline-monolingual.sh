d71d6bc98cb3b2026430e2745b23d2039d29ad1e
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-09-08 11:49:07 +0100

    Initial attempt at translating OCR output to English

---
+++ pipeline-monolingual.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#SBATCH --account t2-cs119-gpu
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task 8
+#SBATCH --gres gpu:4
+#SBATCH --time 24:00:00
+#SBATCH --partition pascal
+set -eou pipefail
+
+export PREFIX=$HOME/src/europat-scripts
+source $PREFIX/init.sh
+
+# How many bleualign?
+export THREADS=${SLURM_CPUS_ON_NODE:-4}
+export TMPSUF=${SLURM_JOB_ID:-$$}
+
+declare -a MODEL=("$1")
+shift
+
+while [[ $# > 0 ]] && [[ $1 != -- ]]; do
+	MODEL=("${MODEL[@]}" "$1")
+	shift
+done
+shift # remove --
+
+# Check for software in path
+which marian-decoder \
+	b64filter \
+	foldfilter \
+	docenc \
+	parallel
+
+col () {
+	cut -d$'\t' -f$1
+}
+
+document_to_base64() {
+	# Remove trailing newline from input (to not cause an empty
+	# document at the end), suffix each line with a null byte,
+	# which will indicate where a document starts. Then inside each
+	# document replace the paragraphs and br tags with newslines.
+	# docenc will then group all of those into base64 encoded chunks.
+	awk 'NR > 1 { print prev } { prev=$0 } END { ORS=""; print }' \
+	| sed -r 's/$/\x0/g' \
+	| sed -r 's/<br\/?>|<\/p><p>/\n/g' \
+	| sed -r 's/<\/?p>//g' \
+	| sed -r 's/@TAB@/ /g' \
+	| docenc -0
+}
+
+preprocess() {
+	if [ ! -z "${LOWERCASE:-}" ]; then
+		echo "Preprocessing with lowercase" >&2
+		lowercase
+	else
+		cat
+	fi
+}
+
+lowercase() {
+	sed -e 's/./\L\0/g'
+}
+
+translate () {
+	b64filter "$@"
+}
+
+for file in $*; do
+	n=$(cat $file | wc -l)
+	echo "$(basename $file): $n documents" >&2
+
+	cat $file \
+	| col 3 \
+	| preprocess \
+	| document_to_base64 \
+	| translate "${MODEL[@]}" \
+	| paste \
+		<(cat $file | col 1) \
+		- \
+	| pigz \
+	> $(basename $file .tab)-en.tab.gz.$TMPSUF \
+	&& mv $(basename $file .tab)-en.tab.gz{.$TMPSUF,}
+done
+
---
+++ translate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+L1=$1
+L2=en
+MODEL=$(dirname $(realpath $0))/models/$L1-$L2
+shift
+
+foldfilter -s -w 500 marian-decoder \
+	--models $MODEL/$L1$L2.base.npz.best-ce-mean-words.npz \
+	--vocabs $MODEL/vocab.$L1$L2.base.spm{,} \
+	--beam-size 6 \
+	--normalize 0.6 \
+	--mini-batch 64 \
+	--maxi-batch 1000 \
+	--maxi-batch-sort src \
+	--workspace 12000 \
+	--quiet-translation \
+	-d ${CUDA_VISIBLE_DEVICES//,/ } \
+	"$@"
